79f5f6ab34c4c84232dbbdee5f8d18f5a84f339e
BarWol <wolnyb12@gmail.com>
2025-12-14 11:42:09 +0100

    delete comment

--- hardware/temperature/temp_sensor_driver.h
+++ hardware/temperature/temp_sensor_driver.h
@@ -3,9 +3,7 @@
 #include "driver/gpio.h"
 #include <stdint.h>
 
-// Initialize DS18B20 on the given GPIO (e.g., GPIO_NUM_4 for D4)
 void temp_sensor_init(gpio_num_t pin);
-
 // Read temperature in Celsius; returns true on success
 bool temp_sensor_read_celsius(float *out_celsius);
 
